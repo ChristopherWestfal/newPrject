bb9d8cd76b03b406ead35ba947236cf03f169d25
ChrizDope <incubusid@googlemail.com>
2024-05-27 17:02:24 +0200

    Add Dependency

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import com.github.lalyos.jfiglet.FigletFont;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-	System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        // using default font standard.flf, obtained from maven artifact
+        String asciiArt1 = FigletFont.convertOneLine("hello world");
+        System.out.println(asciiArt1);
     }
 }
